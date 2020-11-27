1b3c19f72d836768ed5654ada12f7d094d2136fa
chankruze <chankruze@gmail.com>
2020-11-27 10:09:14 +0530

    Finish v1
    Signed-off-by: chankruze <chankruze@gmail.com>

--- routes/proxy.js
+++ routes/proxy.js
@@ -7,7 +7,9 @@ Copyright (c) Geekofia 2020 and beyond
 
 // https://www.restapitutorial.com/httpstatuscodes.html
 
-const router = require("express").Router();
+const router = require("express").Router(),
+  utils = require("../utils"),
+  path = require("path");
 
 router.get("/", async (req, res) => {
   res.status(200).send("Documentation");
@@ -17,30 +19,52 @@ router.post("/encode", async (req, res) => {
   // if API key don't matches (or not found in the DB [prod])
   // return unauthorized status
   if (req.headers["x-cproxy-api-key"] !== process.env.API_KEY) {
-    return res.status(401).json({ status: "failed" });
+    return res.status(401).json(
+      utils.prepareResponse("failed", null, {
+        msg: "request unauthorized",
+      })
+    );
+  }
+
+  // if content-type is not json
+  // return bad request status
+  if (req.headers["content-type"] !== "application/json") {
+    return res.status(400).json(
+      utils.prepareResponse("failed", null, {
+        msg: "only json data allowed",
+      })
+    );
   }
 
   // authorized & proceed proxying
   const { url, timestamp } = req.body;
-  let encodedUrl = "";
 
   try {
     // stuffs to create proxied url
+    const splittedUrl = url.split("/");
+    const filePath = path.resolve(
+      __dirname,
+      "./",
+      `${splittedUrl[splittedUrl.length - 1]}.torrent`
+    );
+    await utils.downloadFile(url, filePath);
+
+    return res.status(200).json(
+      utils.prepareResponse("success", {
+        downloadUrl: filePath,
+      })
+    );
 
     // shorten the encoded url
-    encodedUrl = `${req.headers.host}:${url}`;
+    // encodedUrl = `${req.headers.host}/${file}`;
   } catch (err) {
     console.log(err);
-    return res.status(500).json({
-      status: "failed",
-      error: {
+    return res.status(500).json(
+      utils.prepareResponse("failed", null, {
         msg: "unable to encode requested url",
-        reason: err.toString().split(".")[0],
-      },
-    });
+      })
+    );
   }
-
-  res.status(200).json({ status: "ok", url, encodedUrl });
 });
 
 module.exports = router;
